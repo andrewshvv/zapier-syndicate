6f4efeaea86d43b208f1741e6b3540949f3bbe84
sivaganeshan <bsivaganeshan@hotmail.com>
2022-01-14 23:49:38 +0530

    base contracts

---
+++ contracts/FundContractV1.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//import "./NftFactoryV1.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./NftFactoryV1.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+
+
+
+contract FundContractV1 is Ownable{
+
+    string public Name = "FundContractV1";
+
+    NftFactoryV1 public nftFactoryV1;
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private fundType_;
+
+    constructor(NftFactoryV1  NftFactoryV1_) public {
+        nftFactoryV1 = NftFactoryV1_;
+    }
+
+    uint256 public minimumEthToDepoit = 0.01 ether;
+    
+    struct FundDetails{
+        uint256 fundType;
+        string name;
+        string description;
+        address createdBy;
+        uint256 createdDate;
+        uint256 totalAmountDeposited;
+        uint256 totalAmountDisbursed;
+        uint256 maxDistributableAmountPerTeam;
+        bytes4 credentailsUsed;
+    }
+
+    FundDetails[] internal AllFundDetails  ;
+
+    mapping(uint256=>mapping(address => uint256)) amountDisbursedFromFundByaTeam;
+
+    mapping(uint256=>mapping(address => uint256)) grantProvidedToFundByanInvestor;
+
+    event deposit(address indexed sender, uint256 amount, string message);
+
+    function createFund(
+        string memory name_, 
+        string memory description_, 
+        uint256 distributionAmountPerTram_,
+        bytes4 credentailsUsed_) 
+        external
+        returns(uint256)
+        {
+            fundType_.increment();
+
+            uint256 newFundType = fundType_.current();
+            address createdBy = msg.sender;
+
+            FundDetails memory newFundDetials = FundDetails(
+                newFundType,
+                name_,
+                description_,
+                createdBy,
+                block.timestamp,
+                0,
+                0,
+                distributionAmountPerTram_,
+                credentailsUsed_);
+
+            AllFundDetails.push(newFundDetials);
+
+            return newFundType;
+        }
+
+    function getFundetails() external view returns(FundDetails[] memory){
+
+        return  AllFundDetails;
+    }
+
+    function depositFund(uint256 fundType_) external payable {
+
+         require(
+            msg.value > minimumEthToDepoit,
+            "Requireed minimmum 0.01 Eth to Deposit"
+        );
+
+        bool isDeposited = false;
+        for(uint256 index =1; index < AllFundDetails.length; index++){
+            if(AllFundDetails[index].fundType == fundType_){
+                AllFundDetails[index].totalAmountDeposited = AllFundDetails[index].totalAmountDeposited + msg.value;
+                grantProvidedToFundByanInvestor[fundType_][msg.sender] = 
+                    grantProvidedToFundByanInvestor[fundType_][msg.sender] + msg.value;
+                isDeposited = true;
+            }
+        }
+
+        emit deposit(msg.sender, msg.value, isDeposited?"ETH deposited":"FundTypeNotFound");
+    }
+
+    function evaluavateNFTCredentials( uint256[] memory credentials_) external view returns(uint256[] memory fundtypes_){
+
+        //Ownership verification
+        uint256 credentialLength = credentials_.length;
+        for(uint256 index=0 ; index<credentialLength ; index++)
+        {
+            address ownerAddress = nftFactoryV1.ownerOf(credentials_[index]);
+            require(ownerAddress == msg.sender, "Credentails not owned by msg.sender");
+        }
+        //return matched funddetails 
+        uint256[] memory matchedFundTypes ;
+        for(uint256 index=0 ; index<credentialLength ; index++){
+            bytes4 identifier = nftFactoryV1.getIdentifiers(credentials_[index]);
+
+            for(uint256 indexOFfundType=0 ; indexOFfundType<AllFundDetails.length; indexOFfundType++){
+
+                if(identifier == AllFundDetails[indexOFfundType].credentailsUsed){
+                    matchedFundTypes[index]=AllFundDetails[indexOFfundType].fundType;
+                }
+            }
+        }
+
+        return matchedFundTypes;
+    }
+
+    function getFunding(uint256 credential_, uint256 fundType) external {
+
+        //Ownership verification
+        address ownerAddress = nftFactoryV1.ownerOf(credential_);
+        require(ownerAddress == msg.sender, "Credentails not owned by msg.sender");
+
+        // check for identifiers and transfer
+        bytes4 identifier = nftFactoryV1.getIdentifiers(credential_);
+        for(uint256 indexOFfundType=0 ; indexOFfundType<AllFundDetails.length; indexOFfundType++){
+
+                if(fundType == AllFundDetails[indexOFfundType].fundType){
+
+                     if(identifier == AllFundDetails[indexOFfundType].credentailsUsed){
+                         
+                         require(
+                             grantProvidedToFundByanInvestor[indexOFfundType][msg.sender] <= 0,
+                             "Already Withdrawed for this fund"
+                         );
+                         require(
+                             AllFundDetails[indexOFfundType].totalAmountDeposited - AllFundDetails[indexOFfundType].totalAmountDisbursed > 
+                             AllFundDetails[indexOFfundType].maxDistributableAmountPerTeam,
+                             "Not Enough Fund"
+                         );
+
+                        AllFundDetails[indexOFfundType].totalAmountDisbursed = 
+                            AllFundDetails[indexOFfundType].totalAmountDisbursed + AllFundDetails[indexOFfundType].maxDistributableAmountPerTeam;
+
+                        grantProvidedToFundByanInvestor[indexOFfundType][msg.sender] = AllFundDetails[indexOFfundType].maxDistributableAmountPerTeam;
+                        payable(msg.sender).transfer(AllFundDetails[indexOFfundType].maxDistributableAmountPerTeam);
+                        break;
+                    }
+                }
+               
+            }
+    }
+}
---
+++ contracts/NftFactoryV1.sol
@@ -0,0 +1,124 @@
+
+   
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/Pausable.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/structs/EnumerableSet.sol";
+// import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+
+
+contract NftFactoryV1 is ERC721URIStorage, Ownable, Pausable {
+    
+    using EnumerableSet for EnumerableSet.AddressSet;
+
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+
+    // all the tokens hold by the owner
+    mapping(address => uint256[]) ownerCredentials;
+
+     //Representation of Credential metadata
+    /*
+    * first byte from left refers NFT identity (00 - ETH Global , 01 -Variant Fund , 02-Binance ).
+    * next byte from left refers Event - (00 - WebJam, 01- NFTHAck, 02 - DEFI event and so on).
+    * next byte from left refers the category of the project.(00 - DEFI, 01-NFT, 02-DEX , 03-Analytics ).
+    * next byte from left refers to the seed amount. 
+    * next nible from left refers the place the secured on hackathon. (1- first place, 2 - second place and so on).
+    * next nible from left refers the seed the secured on hackathon. (1- Seed A, 2 - seed B, and so on).
+    */
+    //mapping to get identifier of Token 
+    mapping(uint256 => bytes4) identifiersOfCredential;
+
+    EnumerableSet.AddressSet internal minters;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name,_symbol){
+        minters.add(owner());
+    }
+
+    function isMinter(address _minter) internal view returns (bool _role){
+        return minters.contains(_minter);
+    }
+    
+    // add a minter address can be done only from owner
+    function addMinterRole(address _minter) public onlyOwner{
+        require(_minter != address(this));
+        if(!minters.contains(_minter)){
+            minters.add(_minter);
+        }
+    }
+
+    // remove a minter address can be done only from owner
+    function removeMinterRole(address _minter) public onlyOwner {
+        require(minters.contains(_minter));
+        minters.remove(_minter);
+    }
+   
+
+    function canTransfer() external view returns (bool _status){
+        return (!paused() || _msgSender() == owner());
+    }
+
+    //Note: For current version pass empty tokenURI.
+    //TODO: ADD MAX SUPPLY?
+    //TODO: Add cost for some social capitalist group?
+    function mintAnItem(address to_, string memory tokenURI_,bytes4 credentialIdentifier) external returns(uint256){
+        require(minters.contains(msg.sender),"Sorry you dont have minter role");
+        
+        _tokenIds.increment();
+
+        uint256 newCredential = _tokenIds.current();
+
+        _safeMint(to_,newCredential);
+
+         _setTokenURI(newCredential, tokenURI_);
+
+         ownerCredentials[to_].push(newCredential);
+
+        identifiersOfCredential[newCredential] = credentialIdentifier;
+
+        return newCredential;
+
+    }
+
+  
+    //Implemented hook that ovverides the function to make it nontransferable until desired
+    function _beforeTokenTransfer(address _from, address _to, uint256 _tokenId) internal view override {
+        if (paused()){
+            require(_msgSender() == owner() || minters.contains(_msgSender()), "Only the Owner can Transfer or Mint until pause");
+        }
+    }
+
+    // if a contract is paused only owner() can mint and transfer
+    function pause() public onlyOwner{
+        _pause();
+    }
+
+    //if a contract is unpaused any one having minter role can mint 
+    //Transferable if unpaused
+    function unPause() public onlyOwner{
+        _unpause();
+    }
+    
+    function getCredentials(address owner_) public view returns(uint256[] memory){
+        require(ownerCredentials[owner_].length > 0, "Sorry no Credentials on your wallet");
+        return ownerCredentials[owner_];
+    }
+    
+    function getIdentifiers(uint256 credential_) public view returns(bytes4){
+        return identifiersOfCredential[credential_];
+    }
+
+}
