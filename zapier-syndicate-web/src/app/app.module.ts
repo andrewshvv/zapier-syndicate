c81b93b03c67fdc695d0e33ffaf6172bbb14b27d
Dinesh <dineshse1986@gmail.com>
2022-01-14 22:08:19 -0500

    Basic starter page

--- zapier-syndicate-web/src/app/app.module.ts
+++ zapier-syndicate-web/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { Web3Service } from './web3.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [Web3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
