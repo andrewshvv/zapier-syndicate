e870f1c7a6bdcc29b98b2fd87c91460659068116
Andrey Samokhvalov <andrew.shvv@gmail.com>
2022-01-17 22:00:32 +0300

    last changes, not all tests are working

--- test/Funds.js
+++ test/Funds.js
@@ -1,171 +1,125 @@
 const { expect } = require("chai");
 const { ethers, waffle } = require("hardhat");
 const provider = waffle.provider;
-const { fundHelperWrapper, getHex, tokens, format } = require("./utils.js");
+const {
+  helperGetFundIndexById,
+  helperGetFundDetailsById,
+  helperCreateFund,
+  helperMintAnItem,
+  getHex, tokens, format } = require("./utils.js");
 
 describe("Funds: NFT contract test", () => {
   let credentiaContract;
+  let credentialId;
   let fundContract;
+  let fundId, fundIndex;
+  let fundingAmount;
+
   let admin;
-  let nftOwner1,
-    nftOwner2,
-    nftOwner3,
-    nftOwner4,
-    nftOwner5,
-    nftOwner6,
-    nftOwner7,
-    nftOwner10;
+  let hacker;
+  let receiver;
+  let depositor;
 
   beforeEach(async () => {
     [
       admin,
-      nftOwner1,
-      nftOwner2,
-      nftOwner3,
-      nftOwner4,
-      nftOwner5,
-      nftOwner6,
-      nftOwner7,
-      ...nftOwner10
+      hacker,
+      receiver,
+      depositor
     ] = await ethers.getSigners();
 
     // Deploy nft factory smart contract
-    let credentialFactory = await ethers.getContractFactory("Credentials");
+    const credentialFactory = await ethers.getContractFactory("Credentials");
     credentiaContract = await credentialFactory.deploy("ZAPIERFI", "ZAP");
+
     // Ensure that the event polling is happens faster
     credentiaContract.provider.pollingInterval = 100;
 
     // Deploy fund smart contract
-    let fundFactory = await ethers.getContractFactory("Funds");
-    fundContract = fundHelperWrapper(await fundFactory.deploy(credentiaContract.address));
+    const fundFactory = await ethers.getContractFactory("Funds");
+    fundContract = await fundFactory.deploy(credentiaContract.address);
+
     // Ensure that the event polling is happens faster
     fundContract.provider.pollingInterval = 100;
 
     // Create the nft credential
-    await credentiaContract
-      .connect(admin)
-      .mintAnItem(
-        nftOwner1.address,
-        "https://ipfs.io/ipfs/bafybeibjxyehrtquuybuymgq5iuqll5nqgmqobmilt6fatbxtnpitxebt4/metadata_1.json",
-        getHex("0x00010010")
-      );
-
-
-  });
-
-  it("Verify owner 1 NFT", async () => {
-    let owner = await credentiaContract.ownerOf(1);
-    expect(owner).to.equal(nftOwner1.address);
-  });
-
-  it("Create a fund", async () => {
-    const fundId = await fundContract.helperCreateFund(
-      "name",
-      "description",
-      tokens("0.5"),
-      getHex("0x00010010")
+    const metadata = getHex("0x00010010");
+    const ipfsData = "https://ipfs.io/ipfs/bafybeibjxyehrtquuybuymgq5iuqll5nqgmqobmilt6fatbxtnpitxebt4/metadata_1.json";
+    credentialId = await helperMintAnItem(credentiaContract.connect(admin),
+      receiver.address,
+      ipfsData,
+      metadata
     );
 
-    const fundDetails = await fundContract.helperGetFundDetailsById(fundId);
-    const fundIndex = await fundContract.helperGetFundIndexById(fundDetails.fundId);
+
+    // Initialise fund
+    fundingAmount = tokens("0.5");
+    fundId = await helperCreateFund(fundContract,
+      "name",
+      "description",
+      fundingAmount,
+      metadata // TODO: should be credential type ids
+    );
+
+    // Initialise fund with some funds
+    fundIndex = await helperGetFundIndexById(fundContract, fundId);
+    await fundContract.connect(depositor).depositFunds(fundIndex, { value: 4 * fundingAmount });
+
+  });
+
+  it.only("Verify credential", async () => {
+    // Check test context
+    const owner = await credentiaContract.ownerOf(1);
+    expect(owner).to.equal(receiver.address);
+    expect(credentialId).to.equal(1);
+  });
+
+  it.only("Verify fund has been created", async () => {
+    // Check test context
+    const fundDetails = await helperGetFundDetailsById(fundContract, fundId);
+    const fundIndex = await helperGetFundIndexById(fundContract, fundDetails.fundId);
 
     expect(fundDetails.fundId).to.equal(1);
     expect(fundDetails.name).to.equal("name");
     expect(fundDetails.description).to.equal("description");
-    expect(fundDetails.currentBalance).to.equal(tokens("0"));
-    expect(fundDetails.fundingAmount).to.equal(tokens("0.5"));
+    expect(fundDetails.currentBalance).to.equal(4 * fundingAmount);
+    expect(fundDetails.fundingAmount).to.equal(fundingAmount);
     expect(fundIndex).to.equal(0);
 
     // TODO: check sender
     // TODO: check balance
     // TODO: check credentials types used
-    // TODO: check indexes, ids if many funds added just in case
+
   })
 
-  it("Deposit funds", async () => {
-    const fundId = await fundContract.helperCreateFund(
-      "name",
-      "description",
-      tokens("0.5"),
-      getHex("0x00010010")
-    );
-
-    const fundIndex = await fundContract.helperGetFundIndexById(fundId);
-    await fundContract.depositFunds(fundIndex, { value: tokens("2") });
-
-    const fundDetails = await fundContract.helperGetFundDetailsById(fundId);
+  it.only("Verify funds being deposited", async () => {
+    // Check test context
+    const fundDetails = await helperGetFundDetailsById(fundContract, fundId);
     expect(fundDetails.currentBalance).to.equal(tokens("2"));
   })
 
-  it("Get funding positive test case (NFT owner will get his ETH to his account)", async () => {
-    const fundId = await fundContract.helperCreateFund(
-      "ethglobal",
-      "nft hackathon address",
-      tokens("0.5"),
-      getHex("0x00010010")
+  it.only("Verify nft owner eligable for funding", async () => {
+    // Check test context
+    let fundIds = await fundContract.connect(receiver).eligableFunds([1]);
+    expect(fundIds).to.deep.equal([fundId]);
+  });
+
+  it.only("Test get funding", async () => {
+    // Fund contract balance before calling funding method
+    const contractBalanceBefore = await provider.getBalance(fundContract.address);
+
+    // Credential owner balance before calling funding method
+    const receiverBalanceBefore = await provider.getBalance(receiver.address);
+
+    await fundContract.connect(receiver).getFunding(credentialId, fundIndex);
+
+    // TODO: substitute with check, remove console
+    console.log(
+      "Nft owner Balance after calling GetFunding method : ",
+      format(await provider.getBalance(nftOwner1.address))
     );
 
-    let fundDetails = await fundContract.helperGetFundDetailsById(fundId);
-
-    // TODO: his check should be part of the test initialisation
-    expect(await provider.getBalance(admin.address)).to.equal(tokens("10000"))
-
-    let fundIndex = await fundContract.helperGetFundIndexById(funddetails.fundId);
-    await fundContract.depositFunds(fundIndex, { value: tokens("2") });
-
-    fundDetails = await fundContract.helperGetFundDetailsById(fundId);
-
-    // // TODO: substitute with check, remove console
-    // console.log(funddetails);
-
-    // // TODO: substitute with check, remove console
-    // console.log(
-    //   "admin balance after deposit : ",
-    //   format(await provider.getBalance(admin.address))
-    // );
-
-    // // TODO: substitute with check, remove console
-    // console.log(
-    //   "fund contract Balance before calling Evaluate NFT function : ",
-    //   format(await provider.getBalance(fundContract.address))
-    // );
-
-    // let matchedFundTypes = await fundContract
-    //   .connect(nftOwner1)
-    //   .evaluavateNFTCredentials([1]);
-    // console.log("matched fund types : ", matchedFundTypes);
-
-    // // TODO: substitute with check, remove console
-    // console.log(
-    //   "fund contract Balance after executing Evaluate NFT function : ",
-    //   format(await provider.getBalance(fundContract.address))
-    // );
-
-    // // TODO: substitute with check, remove console
-    // console.log(
-    //   "fund contract Balance before calling GetFunding method : ",
-    //   format(await provider.getBalance(fundContract.address))
-    // );
-
-    // // TODO: substitute with check, remove console
-    // console.log(
-    //   "Nft owner Balance before calling GetFunding method : ",
-    //   format(await provider.getBalance(nftOwner1.address))
-    // );
-
-    // await fundContract.connect(nftOwner1).getFunding(1, 1);
-
-    // // TODO: substitute with check, remove console
-    // console.log(
-    //   "Nft owner Balance after calling GetFunding method : ",
-    //   format(await provider.getBalance(nftOwner1.address))
-    // );
-
-    // // TODO: substitute with check, remove console
-    // console.log(
-    //   "fund contract Balance after executing GetFunding method: ",
-    //   format(await provider.getBalance(fundContract.address))
-    // );
+    // TODO: substitute with check, remove console
   });
 
   it("Get Funding failure Test case (non NFT owner trying to call GetFunding method)", async () => {
--- test/utils.js
+++ test/utils.js
@@ -1,51 +1,62 @@
 const { ethers } = require("hardhat");
 
-module.exports = {
-    getHex: function (hex) {
-        return ethers.utils.arrayify(hex);;
-    },
 
-    tokens: function (val) {
-        return ethers.utils.parseEther(val);
-    },
-
-    format: function (n) {
-        return ethers.utils.formatEther(n);
-    },
-
-    fundHelperWrapper: function (fundContract) {
-
-        // getFundIndexById is helper function to return array index of fund by its id
-        getFundIndexById = async function (fundId) {
-            let allFundDetails = await fundContract.getAllFundDetails()
-            for (let index = 0; index < allFundDetails.length; index++) {
-                if (allFundDetails[index].fundId.toNumber()  == fundId.toNumber()) {
-                    return index;
-                }
-            }
-
-            throw new Error("fund not found");
-        }
-
-        // getFundByDetailsId is helper function to return fund details by its id.
-        getFundDetailsById = async function (fundId) {
-            let fundIndex = await getFundIndexById(fundId)
-            let allFundDetails = await fundContract.getAllFundDetails()
-            return allFundDetails[fundIndex];
-        }
-
-        // createFund version of create fund function which return 
-        // the fund id, by listening to the event.
-        createFund = async function(...args) {
-            const tx = await fundContract.createFund(...args);
-            const receipt = await tx.wait();
-            const event = receipt.events.find(event => event.event === 'FundCreated');
-            return event.args['fundId']
-        }
-
-        fundContract.helperGetFundIndexById = getFundIndexById;
-        fundContract.helperGetFundDetailsById = getFundDetailsById;
-        fundContract.helperCreateFund = createFund;
-        return fundContract;
-    }
+function getHex(hex) {
+    return ethers.utils.arrayify(hex);;
 }
+
+function tokens(val) {
+    return ethers.utils.parseEther(val);
+}
+
+function format(n) {
+    return ethers.utils.formatEther(n);
+}
+
+// getFundIndexById is helper function to return array index of fund by its id
+async function helperGetFundIndexById(fundContract, fundId) {
+    let allFundDetails = await fundContract.getAllFundDetails()
+    for (let index = 0; index < allFundDetails.length; index++) {
+        if (allFundDetails[index].fundId.toNumber() == fundId.toNumber()) {
+            return index;
+        }
+    }
+
+    throw new Error("fund not found");
+}
+
+// getFundByDetailsId is helper function to return fund details by its id.
+async function helperGetFundDetailsById(fundContract, fundId) {
+    let fundIndex = await helperGetFundIndexById(fundContract, fundId)
+    let allFundDetails = await fundContract.getAllFundDetails()
+    return allFundDetails[fundIndex];
+}
+
+
+// mintAnItem version of function which returns the solidity return value from event.
+async function helperMintAnItem(credentialContract, ...args) {
+    const tx = await credentialContract.mintAnItem(...args);
+    const receipt = await tx.wait();
+    const event = receipt.events.find(event => event.event === 'Transfer');
+    return event.args['tokenId']
+}
+
+// createFund version of create fund function which return 
+// the fund id, by listening to the event.
+async function helperCreateFund(fundContract, ...args) {
+    const tx = await fundContract.createFund(...args);
+    const receipt = await tx.wait();
+    const event = receipt.events.find(event => event.event === 'FundCreated');
+    return event.args['fundId']
+
+}
+
+module.exports = {
+    getHex,
+    tokens,
+    format,
+    helperGetFundIndexById,
+    helperGetFundDetailsById,
+    helperCreateFund,
+    helperMintAnItem,
+};
